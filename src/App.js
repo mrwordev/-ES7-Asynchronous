906f344b5aad2cb0686f878d15e392b95e9792c4
Woraprat Jindain <mrrkdesign@gmail.com>
2018-01-30 16:22:01 +0700

    [Add] Firebase example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import logo from './logo.svg';
 import './App.css';
-import tutorialStart from './tutorial/MyApp';
+import tutorialStart, { sendMessage, connectToServer } from './tutorial/MyApp';
 import { getLogs } from './tutorial/MyConsole';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      logs: getLogs()
+      logs: getLogs(),
+      username: '',
+      message: '',
+      messages: []
     };
+    // this.updateData = this.updateData.bind();
+  }
+  updateData(messages) {
+    console.log(messages);
+    this.setState({ messages });
+  }
+  renderMessage(username, message) {
+    return (
+      <div>
+        <span style={{ color: 'green' }}>{username}:</span> {message}
+      </div>
+    );
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="App-col">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <button
+              onClick={() => {
+                connectToServer(this.updateData.bind(this));
+              }}
+            >
+              Connect
+            </button>
+          </div>
+          <div className="App-col Col-right">
+            <div>
+              Username:{' '}
+              <input
+                onChange={event => {
+                  this.setState({ username: event.target.value });
+                }}
+              />
+            </div>
+            <div>
+              Message:{' '}
+              <input
+                onChange={event => {
+                  this.setState({ message: event.target.value });
+                }}
+              />
+            </div>
+            <div>
+              <button
+                onClick={() => {
+                  sendMessage(this.state.username, this.state.message);
+                }}
+              >
+                Send
+              </button>
+            </div>
+          </div>
         </header>
         <button
           onClick={() => {
@@ -29,7 +81,9 @@ class App extends Component {
           Reload{' '}
         </button>
         <div className="App-intro" ref={ref => (this.intro = ref)}>
-          {getLogs().map(log => log)}
+          {_.map(this.state.messages, msg =>
+            this.renderMessage(msg.username, msg.message)
+          )}
         </div>
       </div>
     );
--- src/tutorial/MyApp.js
+++ src/tutorial/MyApp.js
@@ -1,4 +1,33 @@
 import log from './MyConsole';
-export default () => {
-
+import * as firebase from 'firebase';
+const config = {
+  apiKey: 'AIzaSyDDMkY3-O_mEsqTHguy-xcpaaqle7aYn2g',
+  authDomain: 'simya-learning.firebaseapp.com',
+  databaseURL: 'https://simya-learning.firebaseio.com',
+  projectId: 'simya-learning',
+  storageBucket: 'simya-learning.appspot.com',
+  messagingSenderId: '941188548708'
+};
+const firebaseApp = firebase.initializeApp(config);
+export function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export const sendMessage = async (username, message) => {
+  console.log();
+  await firebaseApp
+    .database()
+    .ref(`users/${username}`)
+    .set({ username, message });
+};
+
+export const connectToServer = async callback => {
+  const database = await firebaseApp.database().ref('users');
+  console.log(database);
+  database.on('value', snapshot => {
+    console.log(snapshot);
+    callback(snapshot.val());
+  });
+};
+
+export default async () => {};
